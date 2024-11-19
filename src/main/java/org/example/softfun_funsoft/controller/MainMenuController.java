40453ff3a33fd8b6dd5b6c22236560ca3c086514
loftyyyy <lofty08.business@gmail.com>
2024-11-20 01:02:26 +0800

    addedFurtherController

---
+++ src/main/java/org/example/softfun_funsoft/controller/MainMenuController.java
@@ -0,0 +1,28 @@
+package org.example.softfun_funsoft.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import org.example.softfun_funsoft.model.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainMenuController {
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private ScrollPane leftScroll;
+
+    @FXML
+    private ScrollPane scroll;
+
+
+    private List<Food> foods = new ArrayList<>();
+
+
+
+
+}
---
+++ src/main/java/org/example/softfun_funsoft/model/Food.java
@@ -0,0 +1,13 @@
+package org.example.softfun_funsoft.model;
+
+public class Food {
+    private String name;
+    private String imgSrc;
+    private double price;
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+}
