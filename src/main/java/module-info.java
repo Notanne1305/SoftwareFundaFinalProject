96caf1f7c002ce66e62a348d5c70957ecc2bf76f
Notanne1305 <j.sindo.545117@umindanao.edu.ph>
2024-11-19 22:17:15 +0800

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.example.softfun_funsoft {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+
+    opens org.example.softfun_funsoft to javafx.fxml;
+    exports org.example.softfun_funsoft;
+}
