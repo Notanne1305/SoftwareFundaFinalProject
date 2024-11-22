ebd5ce2757c3c13d8efb7a26a080c795ef72a9c5
loftyyyy <lofty08.business@gmail.com>
2024-11-22 19:06:00 +0800

    changed LauncherName

--- src/main/java/org/example/softfun_funsoft/MainMenuLauncher.java
+++ src/main/java/org/example/softfun_funsoft/MainMenuLauncher.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainMenuLauncher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainMenu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainMenuLauncher.class.getResource("MainMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
